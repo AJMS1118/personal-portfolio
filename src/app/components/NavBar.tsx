279281462a30002884fa76f05b23e8aa2bf36635
Andrew <aj.quesada1990@gmail.com>
2025-03-11 12:40:09 -0400

    creating the side-bar menu

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,38 +1,113 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import Image from "next/image";
-import { assets } from "../../../assets/assets";
+import { assets } from "@/assets/assets";
 
 const NavBar = () => {
+  const sideBarRef = useRef<HTMLUListElement>(null);
+
+  const openSideBar = () => {
+    if (sideBarRef.current) {
+      sideBarRef.current.style.transform = "translateX(-16rem)";
+    }
+  };
+
+  const closeSideBar = () => {
+    if (sideBarRef.current) {
+      sideBarRef.current.style.transform = "translateX(0)";
+    }
+  };
   return (
     <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 traslate-y-[-80%]">
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+      </div>
       <nav className="w-full fixed px-5 lg:px-8 xl:p x-[8%] my-4 flex items-center justify-between z-50">
         <a href="">
           <Image src={assets.logo} alt="" className="w-28 cursor-pointer mr-14" />
         </a>
 
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
-          <li>
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+          <li className="font-Ovo">
             <a href="#top">Home</a>
           </li>
           <li>
-            <a href="#top">About me</a>
+            <a className="font-Ovo" href="#top">
+              About me
+            </a>
           </li>
           <li>
-            <a href="#top">Services</a>
+            <a className="font-Ovo" href="#top">
+              Services
+            </a>
           </li>
           <li>
-            <a href="#top">Contact me</a>
+            <a className="font-Ovo" href="#top">
+              My Work
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#top">
+              Contact me
+            </a>
           </li>
         </ul>
 
         <div>
+          <select name="" id="">
+            <option value="">ES</option>
+            <option value="">EN</option>
+          </select>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <button>
+            <Image src={assets.moon_icon} className="w-6" alt="" />
+          </button>
           <a
             href=""
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border rounded-full border-gray-500  ml-4"
           >
             Contact <Image src={assets.arrow_icon} alt="" className="w-3" />
           </a>
+
+          <button className="block md:hidden" onClick={openSideBar}>
+            <Image src={assets.menu_black} alt="" className="w-6" />
+          </button>
         </div>
+
+        {/*----Mobile menu------*/}
+        <ul
+          ref={sideBarRef}
+          className="flex items-center md:hidden flex-col gap-4 py-20 px-10 fixed top-0 bottom-0 w-64 z-50 -right-64 bg-rose-50 h-screen transition duration-500 ease-in-out"
+        >
+          <div className="absolute top-6 right-6" onClick={closeSideBar}>
+            <Image src={assets.close_black} alt="" className="w-5 cursor-pointer" />
+          </div>
+          <li className="font-Ovo">
+            <a href="#top">Home</a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#top">
+              About me
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#top">
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#top">
+              My Work
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#top">
+              Contact me
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
